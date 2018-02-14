f263afc9e7eef1536cb1b3dbd3b99f41d7190da2
Kathia Rieser <kath.medulla@gmail.com>
2018-02-14 17:14:45 +0100

    front, medio back

--- logic/classifier.js
+++ logic/classifier.js
@@ -9,48 +9,53 @@ var natural_language_understanding = new NaturalLanguageUnderstandingV1({
   'version_date': process.env.VERSION_DATE
 });
 
+
 var a = 0, b;
 
-let params = { 
-  q: '*', 
-  geocode: "40.4893538421231,-3.6827461557,10km",
-  lang: "en",
-  count: 100
+var emotion = {
+    
+    function:function getResult(params){
+
+  return new Promise((resolve, reject) =>{
+  var result;
+  T.get('search/tweets', params).then( (info) => {
+  var twitString = "";
+   let tweets = info.data.statuses;
+   for(let i=0; i< tweets.length; i++){
+     twitString = twitString + " " + tweets[i].text.replace(/[&\/\\#,;.+()$~%.'":*?<>{}]/g,"");
+    }
+    return twitString;
+  }).then((data) => {
+    var parameters = {
+      'text': data,
+      'features': {
+        'entities': {
+          'emotion': true,
+          'sentiment': true,
+          'limit': 1,
+        }
+      }
+    };
+
+    natural_language_understanding.analyze(parameters, function(err, response) {
+      if (err)
+      console.log(err);
+  else
+  var a = 0, b;
+  for (var i = 0; i < 5; i++) { if ( a < Object.values(response.entities[0].emotion)[i]) a = Object.values(response.entities[0].emotion)[i]; b = Object.getOwnPropertyNames(response.entities[0].emotion)[i]; }
+  var result;
+  result = {
+      name: Object.values(response.entities[0].text).join().toString().replace(/,/g,''),
+      emotion: b,
+      value: a
+  }
+  resolve (result);
+   });
+  })
+})
 }
+}
+//getResult(params).then((r)=> console.log(r));
 
-function getResult(params){
+module.exports = emotion; 
 
-	return new Promise((resolve, reject) =>{
-	var result;
-	T.get('search/tweets', params).then( (info) => {
-	var twitString = "";
-	 let tweets = info.data.statuses;
-	 for(let i=0; i< tweets.length; i++){
-		 twitString = twitString + " " + tweets[i].text.replace(/[&\/\\#,;.+()$~%.'":*?<>{}]/g,"");
-		}
-		return twitString;
-	}).then((data) => {
-		var parameters = {
-			'text': data,
-			'features': {
-				'entities': {
-					'emotion': true,
-					'sentiment': true,
-					'limit': 1,
-				}
-			}
-		};
- 
-		natural_language_understanding.analyze(parameters, function(err, response) {
-			if (err)
-			console.log('error:', err);
-		else
-		resolve (response.entities);
-	
-		 });
-		})
- 
- })
- }
- 
- getResult(params).then((r)=> console.log(r));
--- routes/hotpub.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', (req, res, next) => {
-  console.log("Usuario conectado: " + req.user.username);
-  res.render('hotpub/hotpub', { title: 'HotPub' });
-});
-
-module.exports = router;
