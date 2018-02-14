691f9ba97f18ec76243856064b4ae33b96340469
Lucas Martin Calderon <lucasmartincalderon1@gmail.com>
2018-02-14 12:07:03 +0100

    Another commit

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,7 +1,9 @@
+require('../../logic/classifier');
+var JSON = require('../../countries.json');
+
+
 google.charts.load('current', {
   'packages':['geochart'],
-  // Note: you will need to get a mapsApiKey for your project.
-  // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
   'mapsApiKey': 'AIzaSyD1RT6-B1rLTIoZ8nskv8-w1SHBdoP3bsE'
 });
 google.charts.setOnLoadCallback(drawRegionsMap);
