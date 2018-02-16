a3ea627e5b761af8a1f690fbaf3a03f2e1ef8da9
Kathia Rieser <kath.medulla@gmail.com>
2018-02-16 09:43:46 +0100

    crud completado

--- routes/admin.js
+++ routes/admin.js
@@ -57,13 +57,73 @@ router.post('/coordenate/new', checkAdmin, function (req, res, next) {
 
     } else {
       console.log("guardamos coordenada correctamete");
-      res.render('/admin', {
-        title: 'HotPub'
-      });
+      res.redirect('/admin')
     }
 
   });
 
+});
+
+router.get('/coordenate/:id', checkAdmin, (req, res, next) => {
+  Coordenate.findById(req.params.id,(err, coordenate) => {
+    if (err) { return next(err) }
+    if (!coordenate) { return next(new Error("404")) }
+    return res.render('coordenate/view', { coordenate })
+    });
+});
+
+router.get('/coordenate/edit/:id', checkAdmin, (req, res, next) => {
+  console.log("entramos en el get");
+  Coordenate.findById(req.params.id, (err, coordenate) => {
+      if (err) { return next(err) }
+      if (!coordenate) { return next(new Error("404")) }
+      return res.render('coordenate/edit', { coordenate })
+  });
+});
+
+router.post('/coordenate/edit/:id', checkAdmin, (req, res, next) => {
+  
+  console.log("entramos en post");
+  const coordenateId = req.params.id;
+  const newCoordenate = {
+    lat: req.body.latitud,
+    lng: req.body.longitud, 
+    rad: req.body.radio,
+    country: req.body.country,
+    radUnits: req.body.radUnits
+  };
+  console.log(newCoordenate);
+  Coordenate.findByIdAndUpdate(coordenateId, newCoordenate, (err, coordenate) => {
+      if (err) {
+          
+          return res.render('/admin', {
+            coordenate
+              //errors: campaign.errors
+          });
+      }
+      if (!coordenate) {
+          return next(new Error('Error, coordenate does not exist.'));
+      }
+      
+      return res.redirect('/admin');
+  });
+});
+
+router.get('/coordenate/view/:id', checkAdmin, (req, res, next) => {
+  Coordenate.findById(req.params.id, (err, coordenate) => {
+      if (err) { return next(err) }
+      if (!coordenate) { return next(new Error("404")) }
+      return res.render('coordenate/view', { coordenate })
+  });
+});
+
+router.get('/coordenate/delete/:id', (req, res) => {
+  const id = req.params.id;
+
+  Coordenate.findByIdAndRemove(id, (err, product) => {
+    if (err){ return next(err); }
+    return res.redirect('/admin');
+  });
 });
 
 
